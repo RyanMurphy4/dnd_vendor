eb51c3ffd55b26ab3fa070c167e7635c2ef1a072
DeontayPeterson <rnmurphy47@gmail.com>
2024-02-18 14:54:47 -0500

    add class level attributes, create ring function

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 from slots.back import Back
 from slots.chest import Chest
 from slots.foot import Foot
@@ -6,28 +7,49 @@ from slots.head import Head
 from slots.legs import Legs
 from slots.necklace import Necklace
 from slots.ring import Ring
-
-
-
 import random
 
+
 stats = {
-    "Magical Damage Bonus": 2.5,
-    "Additional Magical Damage": 2,
-    'Magical Power': 3,
-    # "Additional Physical Damage": 1,
+    "Magical Damage Bonus": 2.7,
+    "Additional Physical Damage": 2,
+    # "Additional Magical Damage": 1,
+    "Additional Move Speed": 3
+    # 'Magical Power': 3,
     # "Physical Power": 1,
-    
 }
 
-new_ring = Ring(stats)
-decision = new_ring.compare_stats()
+def generate_random_ring(num_stats) -> Ring:
+    max_stats = Ring.max_stats
+    primary_damage_stats = Ring.primary_damage_stats
+    stats = {}
+    i = 0
+    has_2_damage = False
+    while i != num_stats:
+        chosen_stat = random.choice(list(max_stats.keys()))
+        if chosen_stat in stats.keys():
+            continue
+        if chosen_stat in primary_damage_stats:
+            if has_2_damage:
+                print("-- Ring already has a primary damage stat! --")
+                continue
+            else:
+                has_2_damage = True
+        chosen_stat_max = max_stats.get(chosen_stat)
+        if isinstance(chosen_stat_max, float):
+            generated_stat_value = round(random.uniform(0.1, chosen_stat_max), 1)
+            
+        else:
+            generated_stat_value = random.randint(1, chosen_stat_max)
+        stats[chosen_stat] = generated_stat_value
+        i += 1
+    return Ring(stats)
 
-if decision:
-    print("Buy the ring")
-else:
-    print("don't buy the ring")
-
-
+os.system('clear')
+for _ in range(100):
+    temp_ring = generate_random_ring(3)
+    if temp_ring.worth_buying():
+        print(f"{temp_ring=}")
+        print("\n")
 
 
--- slots/ring.py
+++ slots/ring.py
@@ -1,77 +1,108 @@
 class Ring:
-    def __init__(self, item_stats: dict):
+    max_stats = {
+        "Max Health" : 4,
+        "Armor Rating" : 5,
+        "Additional Move Speed": 3,
+        "Move Speed Bonus": 1.0,
+        "Additional Physical Damage": 2,
+        "True Physical Damage": 2,
+        "Additional Magical Damage": 2,
+        "True Magical Damage": 2,
+        "Armor Penetration": 1.5,
+        "Physical Power": 3,
+        "Magical Power": 3,
+        "Magical Damage Bonus": 3.0,
+        "Physical Damage Bonus": 3.0,
+        "Physical Damage Reduction": 1.0,
+        "Action Speed": 1.0,
+        "Max Health Bonus": 2.0,
+        
+    }
+    primary_physical_stat = [
+        'Additional Physical Damage',
+        'True Physical Damage',
+    ]
+
+    secondary_physical_stat = [
+        'Physical Damage Bonus',
+        'Physical Power',
+    ]
+
+    primary_magical_stat = [
+        'Additional Magical Damage',
+        'True Magical Damage',
+    ]
+
+    secondary_magical_stat = [
+        'Magical Power',
+        'Magical Damage Bonus'
+    ]
+    physical_stats = primary_physical_stat + secondary_physical_stat
+    magical_stats = primary_magical_stat + secondary_magical_stat
+    primary_damage_stats = primary_physical_stat + primary_magical_stat
+
+    movement_speed_stats = [
+        'Additional Move Speed',
+        'Movement Speed Bonus',
+    ]
+    def __init__(self, item_stats: dict=None):
         self.item_stats = item_stats
         self.num_stats = len(item_stats)
-        self.max_stats = {
-            "Max Health" : 4,
-            "Armor Rating" : 5,
-            "Additional Move Speed": 3,
-            "Move Speed Bonus": 1.0,
-            "Additional Physical Damage": 2,
-            "True Physical Damage": 2,
-            "Additional Magical Damage": 2,
-            "True Magical Damage": 2,
-            "Armor Penetration": 1.5,
-            "Physical Power": 3,
-            "Magical Power": 3,
-            "Magical Damage Bonus": 3.0,
-            "Physical Damage Bonus": 3.0,
-            "Physical Damage Reduction": 1.0,
-            "Action Speed": 1.0,
-            "Max Health Bonus": 2.0,
-            "Vigor": 0,
-            "Dexterity": 0,
-            "Knowledge": 0,
-            "Strength": 0,
-            "Agility": 0,
-            "Will": 0,
-            "Resourcefulness": 0, 
-        }
+            # "Vigor": 0,
+            # "Dexterity": 0,
+            # "Knowledge": 0,
+            # "Strength": 0,
+            # "Agility": 0,
+            # "Will": 0,
+            # "Resourcefulness": 0, 
 
-        self.magical_stats = [
-            'Additional Magical Damage',
-            'True Magical Damage',
-            'Magical Power',
-            'Magical Damage Bonus'
-        ]
+    
+    def has_max_move_speed(self) -> bool:
+        for stat, value in self.item_stats.items():
+            if stat in self.movement_speed_stats:
+                if (self.max_stats.get(stat) - value) <= .1:
+                    # print("Has max move speed!")
+                    return True
+        return False
 
-        self.physical_stats = [
-            'Additional Physical Damage',
-            'True Physical Damage',
-            'Physical Power',
-            'Physical Damage Bonus'
-        ]
-
-    def compare_stats(self):
-        magic = self.is_magic_ring()
-        physical = self.is_physical_ring()
-        if magic or physical:
-            diff = 0
-            for stat, value in self.item_stats.items():
-                diff += self.max_stats.get(stat) - value
-                print(diff)
-                if diff > .5:
-                    return False
-        return True
+    def has_max_primary_stat(self) -> bool:
+        for stat, value in self.item_stats.items():
+            if stat in self.primary_physical_stat or \
+            stat in self.primary_magical_stat:
                 
+                if not (self.max_stats.get(stat) - value):
+                    # print("Has max primary stat!")
+                    return True
+        return False
+    
+    def has_max_secondary_stat(self) -> bool:
+        for stat, value in self.item_stats.items():
+            if stat in self.secondary_physical_stat or \
+            stat in self.secondary_magical_stat:
+                
+                if (self.max_stats.get(stat) - value) <= .3:
+                    # print("Has max Secondary Stat!")
+                    return True
+        return False
+    
+    def worth_buying(self):
+        return all([
+            self.has_max_primary_stat(),
+            self.has_max_move_speed(),
+            self.has_max_secondary_stat()
+        ])
+    
 
-
-            
-
+    
     def __repr__(self) -> str:
         return f"{self.item_stats}"
-    
-    def is_magic_ring(self):
-        for stat in self.item_stats.keys():
-            if stat not in self.magical_stats:
-                return False
-        return True
-    
-    def is_physical_ring(self):
-        for stat in self.item_stats.keys():
-            if stat not in self.physical_stats:
-                return False
-        return True
 
+'''
+Best ring: Physical power, physical damage bonus, additional physical
+Good ring: +2 phys/mag DAMAGW, +3 mag/phys POWER, +3/+0.9% Move speed/move speed bonus
+For now, skip all other rings.
 
+interaction speed/ action speed/ health/
 
+Can you get +2 healing, +2 mag damage, +3 movement speed?
+'''
