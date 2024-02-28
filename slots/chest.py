33e3521ef88d04586368cafbb5aa52b02f6684cb
DeontayPeterson <rnmurphy47@gmail.com>
2024-02-27 19:03:59 -0500

    Update chest

--- slots/chest.py
+++ slots/chest.py
@@ -19,81 +19,113 @@ class Chest:
         "Will": 2,
         "Resourcefulness": 2,
     }
-    required_physical_stats = [
-        'Max Health',
+
+    physical_stats = [
         'Physical Power',
-        'Max Health Bonus',
         'Physical Damage Bonus',
     ]
 
-    # Won't be used until gold, mystic vestments isn't worth.
-    required_magical_stats = [
+    health_stats = [
         'Max Health',
         'Max Health Bonus',
-        'Magical Power',
-        "Magical Damage Bonus"
     ]
 
-    comp_physical_stats = [
-        "Action Speed",
-    ]
-    
-    acceptable_physical_stats = [
-        'Vigor',
+    comp_stats_tripelt = [
         'Dexterity',
-        'Strength',
+        'Action Speed',
     ]
+
+    comp_stats_rubysilver = [
+        'Dexterity',
+        'Action Speed',
+        'Vigor',
+    ]
+
+    rubysilver_attr = [
+        
+    ] 
     
     def __init__(self, item_stats: dict=None, item_name: str=None):
         self.item_name = item_name
-        self.item_stats = item_stats.get('random_stats', None)
-        self.num_stats = len(item_stats.get('random_stats', ''))
+        self.random_stats = item_stats.get('random_stats', None)
+        self.num_random_stats = len(item_stats.get('random_stats', ''))
         self.static_stats = item_stats.get('static_stats', None)
-        self.num_static_stats = len(item_stats.get('static_stats'), '')
+        self.num_static_stats = len(item_stats.get('static_stats', ''))
          
-    def check_primary_stats(self):
-        num_prim_stats = 0
-        for stat, value in self.item_stats.items():
-            if stat in self.required_physical_stats:
-                if (self.max_stats.get(stat) - value) < .5:
-                    num_prim_stats += 1
-        return num_prim_stats
+    def check_static_health(self, threshold) -> bool:
+        health_value = self.static_stats.get('Max Health Bonus')
+        max_value = 5.0
+
+        if (max_value - health_value) <= threshold:
+            return True
+        return False
     
-    def check_static_stats(self):
-        num_static_stats = 0
-        for stat, value in self.static_stats.items():
-            if stat == 'Max Health Bonus': # Max health is the only static stat that changes.
-                highest = 5.0
-                if (highest - value) <= 1.2:
-                    num_static_stats += 1
-            else:
-                num_static_stats += 1
-        return num_static_stats
-
-    def check_comp_physical_stats(self):
-        comp_phys_stats = 0
-        for stat, value in self.item_stats.items():
-            if stat in self.comp_physical_stats:
-                if (self.max_stats.get(stat) - value) <= .3:
-                    comp_phys_stats += 1
-        return comp_phys_stats
+    def check_stats(self, stat_list: list[str], threshold: int, f_threshold: float) -> int:
+        '''
+        threshold: Maximum difference allowed between stats that are type integer. Example: Physical Power
+        f_threshold: Maximum difference allowed between stats that are type float. Example: Max Health Bonus
+        
+        returns: Number of stats that are within the threshold.
+        '''
+        num_stats = 0
+        chosen_stats = []
+        for stat, value in self.random_stats.items():  
+            if stat in stat_list:
+                if isinstance(value, float):
+                    if (self.max_stats.get(stat) - value) <= f_threshold:
+                        num_stats += 1
+                        chosen_stats.append(stat)
+                    
+                elif (self.max_stats.get(stat) - value) <= threshold:
+                    num_stats += 1
+                    chosen_stats.append(stat)
+        return num_stats, chosen_stats
     
-    def check_acceptable_physical_stats(self):
-        acceptable_phys_stats = 0
-        for stat, value in self.item_stats.items():
-            if stat in self.acceptable_physical_stats:
-                if (self.max_stats.get(stat) - value) == 0:
-                    acceptable_phys_stats += 1
+    def buy_tripelt(self) -> bool:
+        if not self.check_static_health(1.2):
+            return False
+        
+        num_phys_stats, phys_stats = self.check_stats(self.physical_stats, 0, .5)
+        num_health_stats, phys_stats = self.check_stats(self.health_stats, 1, 0)
+        num_comp_stats, comp_stats = self.check_stats(self.comp_stats_tripelt, 0, .2)
 
-        return acceptable_phys_stats
+        if num_phys_stats == 2: # Phys damage AND Phys power
+            return True
+        if num_phys_stats + num_health_stats == 2: # (phys damage OR phys power) AND (max health)
+            return True
+        if (num_phys_stats == 1 or num_health_stats == 1) and num_comp_stats == 1: # (Phys damage or Phys power) or (max health) AND (dex OR action speed)
+            return True
+        return False
+    
+    def buy_ruby_doublet(self) -> bool:
+        num_phys_stats, phys_stats = self.check_stats(self.physical_stats, 0, .5)
+        num_health_stats, health_stats = self.check_stats(self.health_stats, 1, .5)
+        num_comp_stats , comp_stats = self.check_stats(self.comp_stats_rubysilver, 0, .3)
 
+        
+        if num_phys_stats == 2:
+            if num_health_stats or num_comp_stats:
+                return True
+            if "Vigor" in list(self.random_stats.keys()):
+                if self.random_stats.get('Vigor') == 2:
+                    return True
+            
+        if num_health_stats == 2:
+            if num_phys_stats or num_comp_stats:
+                return True
+        
+        return False
+         
     def worth_buying(self):
-        ...
+        if self.item_name == 'tri-pelt doublet':
+            return self.buy_tripelt()
+            
+        elif self.item_name == 'rubysilver doublet':
+            return self.buy_ruby_doublet()
         
-
-        
-        
+        return False
+            
     def __repr__(self) -> str:
-        return f"{self.item_stats}"
+        return f"Name: {self.item_name}\nrandom_stats={self.random_stats}\nstatic_stats={self.static_stats}"
 
     
